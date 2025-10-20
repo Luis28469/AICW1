97db0c2497fea6dcbf90a52d2743c6836becd396
Wen Liu <wentaoliu0814@gmail.com>
2025-10-20 21:29:33 +0100

    added hard mode Basically increases rate of 2/3 appearing and ensures there arent any hingers in the next 3 moves

--- a4_game.py
+++ a4_game.py
@@ -1,3 +1,4 @@
+from collections import deque
 from a1_state import State
 from a2_path import path_BFS, path_astar, path_IDDFS, path_DFS
 from a3_agent import Agent
@@ -32,7 +33,7 @@ def play(state, agent1, agent2):
         print(state)
         #for draws
         if all(cell==0 for row in state.grid for cell in row):
-            print("Draw - All Cells Removed")
+            print("Draw - All Cells Removed\n")
             return None
         
         #get current player name (checking if its an agent or human)
@@ -70,10 +71,52 @@ def play(state, agent1, agent2):
         if state.get_winner() is not None:
             print("\nHINGER CREATED, GAME OVER")
             print(f"\n{player_name} Wins!")
+            print("Here is the final board state: \n")
+            print(state)
+            print("\n")
             return player_name
 
         current_player_id = (current_player_id + 1) % 2
         
+
+#uses BFS to search all possible moves on the board move by move to check if the board wont have any hingers in the next n moves
+def is_safe_for_n_moves(initial_state, max_depth):
+    queue = deque([(initial_state, 0)])
+    visited = {tuple(map(tuple, initial_state.grid))}
+
+    while queue:
+        current_state, depth = queue.popleft()
+
+        if current_state.numHingers() > 0:
+            return False
+        
+        if depth < max_depth:
+            for move in current_state.moves():
+                new_grid = copy.deepcopy(current_state.grid)
+                new_grid[move[0]][move[1]] -= 1
+                next_state = State(new_grid)
+                new_grid_tuple = tuple(map(tuple, next_state.grid))
+
+                if new_grid_tuple not in visited:
+                    visited.add(new_grid_tuple)
+                    queue.append((State(new_grid), depth + 1))
+
+    return True
+
+
+#code for hard mode, changes the weighting of each cell in the grid to have a higher chance of having a number >1
+def hard_mode(rows,cols):
+    while True:
+        grid = [[random.choices([0, 1, 2, 3], weights=[0.15, 0.3, 0.35, 0.2], k=1)[0] for _ in range(cols)]for _ in range(rows)]
+
+        initial_state = State(grid)
+        
+        if is_safe_for_n_moves(initial_state, 3):
+            print("------------------")
+            print("Safe Grid Found! Starting Game")
+            print("------------------\n")
+            return grid
+
 
 
 
@@ -86,8 +129,23 @@ def tester():
     # ]
     rows, columns = 4, 5
     #random grid
-    start_grid = [[random.choices([0, 1, 2, 3], weights=[0.4, 0.3, 0.2, 0.1 ], k=1)[0] for _ in range(columns)]
-        for _ in range(rows)]
+    # start_grid = [[random.choices([0, 1, 2, 3], weights=[0.4, 0.3, 0.2, 0.1 ], k=1)[0] for _ in range(columns)]for _ in range(rows)]
+    start_grid = None
+
+    #select difficulty
+    while True:
+        difficulty = input("Select Difficulty:\nEASY:1\nHARD:2\n")
+        if difficulty == "1":
+            start_grid = [[random.choices([0, 1, 2, 3], weights=[0.4, 0.3, 0.25, 0.05 ], k=1)[0] for _ in range(columns)]for _ in range(rows)]
+            break
+        if difficulty == "2":
+            print("Generating Hard Mode Grid...\n")
+            start_grid = hard_mode(rows,columns)
+            break
+        if difficulty == "exit":
+            exit()
+        else:
+            print("Invalid Input")
     
     game_state = State(start_grid)
     agent1 = Agent(size=(4, 5), name="AIAgent1")
@@ -102,17 +160,19 @@ def tester():
         name2 = input("Enter Player 2 Name: ")
         winner = play(copy.deepcopy(game_state),name1, name2)
         return winner
-    if Gamemode == "2":
+    elif Gamemode == "2":
         #MODE 2
         print("\nMODE 2: HUMAN VS AI")
         name1 = input("Enter Player 1 Name: ")
         winner = play(copy.deepcopy(game_state),name1, agent2)
         return winner
-    if Gamemode == "3":
+    elif Gamemode == "3":
         #MODE 3
         print("\nMODE 3: AI VS AI")
         winner = play(copy.deepcopy(game_state),agent1, agent2)
         return winner
+    else:
+        print("Invalid Input")
 
 
 if __name__ == "__main__":
